58c7b3f078f7c91b62b1b70ceb1e2a68ed5c69cc
Starinets <tsn.ipad@gmail.com>
2020-07-31 15:11:54 +0600

    markup trip main info, trip cost template

--- src/main.js
+++ src/main.js
@@ -12,16 +12,25 @@ const tripSortingPlace = document.querySelector('.js-sorting');
 const createTripInfoTemplate = () => {
   return (
     `<section class="trip-main__trip-info  trip-info">
-      <div class="trip-info__main">
-        <h1 class="trip-info__title">Amsterdam &mdash; Chamonix &mdash; Geneva</h1>
-
-        <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;20</p>
-      </div>
-
-      <p class="trip-info__cost">
-        Total: &euro;&nbsp;<span class="trip-info__cost-value">1230</span>
-      </p>
     </section>`
+  );
+};
+
+const createTripMainInfoTemplate = () => {
+  return (
+    `<div class="trip-info__main">
+      <h1 class="trip-info__title">Amsterdam &mdash; Chamonix &mdash; Geneva</h1>
+
+      <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;20</p>
+    </div>`
+  );
+};
+
+const createTripCostInfoTemplate = () => {
+  return (
+    `<p class="trip-info__cost">
+      Total: &euro;&nbsp;<span class="trip-info__cost-value">1230</span>
+    </p>`
   );
 };
 
@@ -273,6 +282,7 @@ render(tripMenuPlace, createMenuTemplate(), 'afterend');
 render(tripFiltersPlace, createFiltersTemplate(), 'beforeend');
 
 render(tripSortingPlace, createTripSortTemplate(), 'afterend');
+render(tripContentPlace, createEventFormTemplate(), 'beforeend');
 render(tripContentPlace, createTripDaysTemplate(), 'beforeend');
 
 const tripDayPlace = tripContentPlace.querySelector('.trip-days');
