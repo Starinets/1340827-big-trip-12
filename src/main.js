f7203bd978fb1f7679d5844c06e2c48b592756fd
Starinets <tsn.ipad@gmail.com>
2020-07-31 12:57:50 +0600

    markup Menu component

--- src/main.js
+++ src/main.js
@@ -16,6 +16,15 @@ const createTripInfoTemplate = () => {
   );
 };
 
+const createMenuTemplate = () => {
+  return (
+    `<nav class="trip-controls__trip-tabs  trip-tabs">
+      <a class="trip-tabs__btn  trip-tabs__btn--active" href="#">Table</a>
+      <a class="trip-tabs__btn" href="#">Stats</a>
+    </nav>`
+  );
+};
+
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
