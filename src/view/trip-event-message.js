8deff47c3b6f63b0413dbcaf271bd8d9f592dac4
Starinets <tsn.ipad@gmail.com>
2020-08-01 12:49:19 +0600

    markup trip event message

---
+++ src/view/trip-event-message.js
@@ -0,0 +1,7 @@
+const createTripEventMessageTemplate = () => {
+  return (
+    `<p class="trip-events__msg">Click New Event to create your first point</p>`
+  );
+};
+
+export {createTripEventMessageTemplate};
