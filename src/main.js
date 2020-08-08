b6ad924169ef96754ab87c2401c4243a06ec5e9b
Starinets <tsn.ipad@gmail.com>
2020-08-08 14:37:12 +0600

    use constants in render call

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import {render} from './utils/dom.js';
+import {insertPosition} from './utils/constants.js';
 import {createInfoTemplate} from './view/info.js';
 import {createMainInfoTemplate} from './view/main-info.js';
 import {createCostInfoTemplate} from './view/cost-info.js';
@@ -21,28 +22,28 @@ const filtersPlace = infoPlace.querySelector(`.trip-controls`);
 const contentPlace = document.querySelector(`.trip-events`);
 const sortingPlace = document.querySelector(`.js-sorting`);
 
-render(infoPlace, createInfoTemplate(), `afterbegin`);
-render(infoPlace, createAddPointButtonTemplate(), `beforeend`);
+render(infoPlace, createInfoTemplate(), insertPosition.AFTER_BEGIN);
+render(infoPlace, createAddPointButtonTemplate(), insertPosition.BEFORE_END);
 
 const infoMainPlace = infoPlace.querySelector(`.trip-info`);
-render(infoMainPlace, createMainInfoTemplate(), `beforeend`);
-render(infoMainPlace, createCostInfoTemplate(), `beforeend`);
+render(infoMainPlace, createMainInfoTemplate(), insertPosition.BEFORE_END);
+render(infoMainPlace, createCostInfoTemplate(), insertPosition.BEFORE_END);
 
 
-render(menuPlace, createMenuTemplate(), `afterend`);
-render(filtersPlace, createFiltersTemplate(), `beforeend`);
+render(menuPlace, createMenuTemplate(), insertPosition.AFTER_END);
+render(filtersPlace, createFiltersTemplate(), insertPosition.BEFORE_END);
 
-render(sortingPlace, createSortTemplate(), `afterend`);
-render(contentPlace, createDaysTemplate(), `beforeend`);
+render(sortingPlace, createSortTemplate(), insertPosition.AFTER_END);
+render(contentPlace, createDaysTemplate(), insertPosition.BEFORE_END);
 
 const dayPlace = contentPlace.querySelector(`.trip-days`);
-render(dayPlace, createDayTemplate(), `beforeend`);
+render(dayPlace, createDayTemplate(), insertPosition.BEFORE_END);
 const pointListPlace = contentPlace.querySelector(`.day`);
-render(pointListPlace, createPointListTemplate(), `beforeend`);
+render(pointListPlace, createPointListTemplate(), insertPosition.BEFORE_END);
 
 const eventPlace = dayPlace.querySelector(`.trip-events__list`);
 
-render(eventPlace, createPointFormTemplate(), `beforeend`);
+render(eventPlace, createPointFormTemplate(), insertPosition.BEFORE_END);
 for (let i = 0; i < EVENT_COUNT; i++) {
-  render(eventPlace, createPointTemplate(), `beforeend`);
+  render(eventPlace, createPointTemplate(), insertPosition.BEFORE_END);
 }
