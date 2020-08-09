07a11cc3d3f38ad79447282c7afacaa028b85c66
Starinets <tsn.ipad@gmail.com>
2020-08-09 15:37:21 +0600

    add destination info (and photos) to event structure

--- src/mock/point.js
+++ src/mock/point.js
@@ -1,6 +1,8 @@
 import {getRandomInteger} from '../utils/random.js';
 
 const OFFERS_COUNT = 5;
+const INFO_COUNT = 5;
+const PHOTOS_COUNT = 5;
 
 const pointType = [
   `Taxi`,
@@ -26,10 +28,28 @@ const destinations = [
   `Ottava`
 ];
 
+const infos = [
+  `Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
+  `Cras aliquet varius magna, non porta ligula feugiat eget.`,
+  `Fusce tristique felis at fermentum pharetra.`,
+  `Aliquam id orci ut lectus varius viverra.`,
+  `Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.`,
+  `Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.`,
+  `Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.`,
+  `Sed sed nisi sed augue convallis suscipit in sed felis.`,
+  `Aliquam erat volutpat.`,
+  `Nunc fermentum tortor ac porta dapibus.`,
+  `In rutrum ac purus sit amet tempus.`
+];
+
 const generatePoint = () => ({
   type: pointType[getRandomInteger(0, pointType.length - 1)],
   destination: destinations[getRandomInteger(0, destinations.length - 1)],
   offers: generateOffers(getRandomInteger(0, OFFERS_COUNT)),
+  destinationInfo: {
+    text: generateDestinationInfo(getRandomInteger(1, INFO_COUNT)),
+    photos: generateDestinationPhotos(getRandomInteger(1, PHOTOS_COUNT)),
+  },
 });
 
 const generateOffers = (offersCount) => new Array(offersCount)
@@ -39,5 +59,14 @@ const generateOffers = (offersCount) => new Array(offersCount)
     name: `offerName`,
     price: `offerPrice`,
   }));
+
+const generateDestinationInfo = (infoCount) => new Array(infoCount)
+  .fill()
+  .map(() => infos[getRandomInteger(0, infos.length - 1)])
+  .join(` `);
+
+const generateDestinationPhotos = (photosCount) => new Array(photosCount)
+  .fill()
+  .map(() => Math.random());
 
 export {generatePoint};
