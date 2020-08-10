4e05431d155a602c9d19b6d3e22b8a914b97c1ce
Starinets <tsn.ipad@gmail.com>
2020-08-10 12:36:11 +0600

    add offers to event structure

--- src/mock/point.js
+++ src/mock/point.js
@@ -1,15 +1,13 @@
-import {getRandomInteger, getRandomDate} from '../utils/random.js';
+import {
+  getRandomInteger,
+  getRandomDate
+} from '../utils/random.js';
 
 const OFFERS_COUNT = 5;
 const INFO_COUNT = 5;
 const PHOTOS_COUNT = 5;
-const Price = {
-  min: 2,
-  max: 50,
-  multiplicity: 10,
-};
 
-const pointType = [
+const POINT_TYPES = [
   `Taxi`,
   `Bus`,
   `Train`,
@@ -22,7 +20,7 @@ const pointType = [
   `Restaurant`
 ];
 
-const destinations = [
+const DESTINATIONS = [
   `Amsterdam`,
   `Frankfurt`,
   `Stambul`,
@@ -33,7 +31,7 @@ const destinations = [
   `Ottava`
 ];
 
-const infos = [
+const INFOS = [
   `Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
   `Cras aliquet varius magna, non porta ligula feugiat eget.`,
   `Fusce tristique felis at fermentum pharetra.`,
@@ -47,16 +45,50 @@ const infos = [
   `In rutrum ac purus sit amet tempus.`
 ];
 
+const OFFERS = [
+  {
+    type: `luggage`,
+    name: `Add luggage`,
+    price: 30
+  },
+  {
+    type: `comfort`,
+    name: `Switch to comfort class`,
+    price: 100
+  },
+  {
+    type: `meal`,
+    name: `Add meal`,
+    price: 15
+  },
+  {
+    type: `seats`,
+    name: `Choose seats`,
+    price: 5
+  },
+  {
+    type: `train`,
+    name: `Travel by train`,
+    price: 40
+  }
+];
+
+const Price = {
+  min: 2,
+  max: 50,
+  multiplicity: 10,
+};
+
 const generatePoint = (minDate = new Date()) => {
   let point = {
-    type: pointType[getRandomInteger(0, pointType.length - 1)],
-    destination: destinations[getRandomInteger(0, destinations.length - 1)],
+    type: POINT_TYPES[getRandomInteger(0, POINT_TYPES.length - 1)],
+    destination: DESTINATIONS[getRandomInteger(0, DESTINATIONS.length - 1)],
     offers: generateOffers(getRandomInteger(0, OFFERS_COUNT)),
     destinationInfo: {
       text: generateDestinationInfo(getRandomInteger(1, INFO_COUNT)),
       photos: generateDestinationPhotos(getRandomInteger(1, PHOTOS_COUNT)),
     },
-    cost: getRandomInteger(Price.min, Price.max) * Price.multiplicity,
+    price: getRandomInteger(Price.min, Price.max) * Price.multiplicity,
   };
 
   minDate = getRandomDate(minDate);
@@ -69,15 +101,11 @@ const generatePoint = (minDate = new Date()) => {
 
 const generateOffers = (offersCount) => new Array(offersCount)
   .fill()
-  .map(() => ({
-    type: `offerType`,
-    name: `offerName`,
-    price: `offerPrice`,
-  }));
+  .map(() => OFFERS[getRandomInteger(0, OFFERS.length - 1)]);
 
 const generateDestinationInfo = (infoCount) => new Array(infoCount)
   .fill()
-  .map(() => infos[getRandomInteger(0, infos.length - 1)])
+  .map(() => INFOS[getRandomInteger(0, INFOS.length - 1)])
   .join(` `);
 
 const generateDestinationPhotos = (photosCount) => new Array(photosCount)
