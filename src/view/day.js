e409e159cb4ee6ff7082e42a72d043b2b09908cb
Starinets <tsn.ipad@gmail.com>
2020-08-15 04:28:25 +0600

    class PointList

--- src/view/day.js
+++ src/view/day.js
@@ -1,20 +1,20 @@
 import {createElement} from './../utils/dom';
-import {createPointListTemplate} from './point-list';
+import PointList from './point-list';
 
 const formatMonthDate = new Intl.DateTimeFormat(`en-GB`, {
   month: `short`,
   day: `numeric`,
 }).format;
 
-const createDayTemplate = (dayDate, counter, dayPoints) => {
+const createDayTemplate = (dayDate, counter) => {
   return (
     `<li class="trip-days__item  day">
       <div class="day__info">
         <span class="day__counter">${counter}</span>
         <time class="day__date" datetime="${dayDate.toISOString()}">${formatMonthDate(dayDate)}</time>
       </div>
-      ${createPointListTemplate(dayPoints)}
     </li>`
+    // ${createPointListTemplate(dayPoints)}
   );
 };
 
@@ -31,6 +31,8 @@ export default class Day {
     if (!this._element) {
       this._element = createElement(this._getTemplate(dayDate, counter, dayPoints));
     }
+
+    this._element.append(new PointList().getElement(dayPoints));
 
     return this._element;
   }
--- src/view/point-list.js
+++ src/view/point-list.js
@@ -1,3 +1,4 @@
+import {createElement} from './../utils/dom';
 import {createPointTemplate} from './point.js';
 
 const generatePointsTemplates = (points) => points
@@ -9,5 +10,27 @@ const createPointListTemplate = (points) => {
     ${generatePointsTemplates(points)}
   </ul>`;
 };
+
+export default class PointList {
+  constructor() {
+    this._element = null;
+  }
+
+  _getTemplate(dayPoints) {
+    return createPointListTemplate(dayPoints);
+  }
+
+  getElement(dayPoints) {
+    if (!this._element) {
+      this._element = createElement(this._getTemplate(dayPoints));
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
 
 export {createPointListTemplate};
