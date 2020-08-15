f15bc073bd30abc5eef2ee62c09bb757e7e6f3eb
Starinets <tsn.ipad@gmail.com>
2020-08-15 06:16:01 +0600

    refactor DOM render function

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -1,22 +1,36 @@
-const InsertPosition = {
+const RenderPosition = {
   BEFORE_BEGIN: `beforebegin`,
   AFTER_BEGIN: `afterbegin`,
   BEFORE_END: `beforeend`,
   AFTER_END: `afterend`
 };
 
-export const createElement = (template) => {
+const createElement = (template) => {
   const newElement = document.createElement(`div`);
   newElement.innerHTML = template;
 
   return newElement.firstChild;
 };
 
-const render = (container, template, place) => {
-  container.insertAdjacentHTML(place, template);
+const render = (container, element, place) => {
+  switch (place) {
+    case RenderPosition.BEFORE_BEGIN:
+      container.before(element);
+      break;
+    case RenderPosition.AFTER_BEGIN:
+      container.prepend(element);
+      break;
+    case RenderPosition.BEFORE_END:
+      container.append(element);
+      break;
+    case RenderPosition.AFTER_END:
+      container.after(element);
+      break;
+  }
 };
 
 export {
+  createElement,
   render,
-  InsertPosition
+  RenderPosition
 };
