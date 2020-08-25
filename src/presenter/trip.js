4a76eea9aa8830bf5ba99466ca237ea017f9014d
Starinets <tsn.ipad@gmail.com>
2020-08-25 16:35:54 -0400

    Presenter declaration

---
+++ src/presenter/trip.js
@@ -0,0 +1,20 @@
+
+export default class trip {
+  constructor() {}
+
+  init() {}
+
+  _renderNoPointMessage() {}
+
+  _renderDaysList() {}
+
+  _renderDay() {}
+
+  _renderPointsList() {}
+
+  _renderPoint() {}
+
+  _renderOffersList() {}
+
+  _renderOffer() {}
+}
