c7a8f3407e50a46058f8c90601b21ca615139441
Starinets <tsn.ipad@gmail.com>
2020-09-01 19:51:29 -0400

    use moment for duration calculate

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,20 +1,15 @@
 import moment from "moment";
 
-const MILLISECOND_IN_MINUTE = 60000;
-const MINUTES_IN_HOUR = 60;
-const HOURS_IN_DAY = 24;
-
-
 const addLeadingRank = (value) => {
   return String(value).padStart(2, `0`);
 };
 
 const dateToString = (date) => {
-  return moment(date).format(`DD/MM/YY hh:mm`);
+  return moment(date).format(`DD/MM/YY HH:mm`);
 };
 
 const timeToString = (date = new Date()) => {
-  return moment(date).format(`hh:mm`);
+  return moment(date).format(`HH:mm`);
 };
 
 const formatDateToISOString = (date) => {
@@ -23,16 +18,11 @@ const formatDateToISOString = (date) => {
 
 const formatDayDate = (date) => moment(date).format(`DD/MM/YYYY`);
 
-const getDatesDifference = (startDate = new Date(), endDate = new Date()) => {
-  let difference = endDate - startDate;
-  difference -= difference % MILLISECOND_IN_MINUTE;
-  difference = difference / MILLISECOND_IN_MINUTE;
-  const minutes = difference % MINUTES_IN_HOUR;
-  difference -= minutes;
-  difference = difference / MINUTES_IN_HOUR;
-  const hours = difference % HOURS_IN_DAY;
-  difference -= hours;
-  const days = difference / HOURS_IN_DAY;
+const getDatesDifference = (startTime = new Date(), endTime = new Date()) => {
+  const duration = moment.duration(moment(endTime).diff(moment(startTime)));
+  const minutes = duration.minutes();
+  const hours = duration.hours();
+  const days = duration.days();
 
   let formattedTime = ``;
 
@@ -49,11 +39,14 @@ const getDatesDifference = (startDate = new Date(), endDate = new Date()) => {
   return formattedTime;
 };
 
+const formatMonthDate = (date) => moment(date).format(`MMM D`);
+
 export {
   addLeadingRank,
   getDatesDifference,
   dateToString,
   timeToString,
   formatDateToISOString,
-  formatDayDate
+  formatDayDate,
+  formatMonthDate
 };
