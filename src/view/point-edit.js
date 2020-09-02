5cc66dca6cd5849e38f73f29884675436c231dcc
Starinets <tsn.ipad@gmail.com>
2020-09-02 12:29:49 -0400

    change price field type text -> number

--- src/view/point-edit.js
+++ src/view/point-edit.js
@@ -157,7 +157,7 @@ const createPointFormTemplate = (pointData, destinations) => {
             <span class="visually-hidden">Price</span>
             €
           </label>
-          <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="${pointData.price}">
+          <input class="event__input  event__input--price" id="event-price-1" type="number" name="event-price" value="${pointData.price}">
         </div>
 
         <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
