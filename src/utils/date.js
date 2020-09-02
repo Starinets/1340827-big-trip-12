7799f6f6a1833936a1c6f362059d921113ffd3ec
Starinets <tsn.ipad@gmail.com>
2020-09-02 12:14:07 -0400

    refactor utils/date

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,45 +1,28 @@
-import moment from "moment";
+import moment, {duration} from "moment";
 
-const addLeadingRank = (value) => {
-  return String(value).padStart(2, `0`);
-};
+const addLeadingRank = (value) => String(value).padStart(2, `0`);
 
-const dateToString = (date) => {
-  return moment(date).format(`DD/MM/YY HH:mm`);
-};
+const dateToString = (date) => moment(date).format(`DD/MM/YY HH:mm`);
 
-const timeToString = (date = new Date()) => {
-  return moment(date).format(`HH:mm`);
-};
+const timeToString = (date) => moment(date).format(`HH:mm`);
 
-const formatDateToISOString = (date) => {
-  return moment(date).format();
-};
+const formatDateToISOString = (date) => moment(date).format();
 
 const formatDayDate = (date) => moment(date).format(`DD/MM/YYYY`);
 
-const getDatesDifference = (startTime = new Date(), endTime = new Date()) => {
-  const duration = moment.duration(moment(endTime).diff(moment(startTime)));
-  const minutes = duration.minutes();
-  const hours = duration.hours();
-  const days = duration.days();
-
-  let formattedTime = ``;
-
-  if (days > 0) {
-    formattedTime = addLeadingRank(days) + `D `;
-  }
-
-  if (days > 0 || hours > 0) {
-    formattedTime = formattedTime + addLeadingRank(hours) + `H `;
-  }
-
-  formattedTime = formattedTime + addLeadingRank(minutes) + `M`;
-
-  return formattedTime;
-};
-
 const formatMonthDate = (date) => moment(date).format(`MMM D`);
+
+const getDatesDifference = (startDate, endDate) => {
+  const {days, hours, minutes} = duration(endDate - startDate)._data;
+
+  return `${
+    days > 0 ? addLeadingRank(days) + `D ` : ``
+  }${
+    days > 0 || hours > 0 ? addLeadingRank(hours) + `H ` : ``
+  }${
+    addLeadingRank(minutes) + `M`
+  }`;
+};
 
 export {
   addLeadingRank,
