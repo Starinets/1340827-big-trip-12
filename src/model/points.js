740ba1bf372f21d88be66ac6f9427ea1115e4711
Starinets <tsn.ipad@gmail.com>
2020-09-04 00:07:38 -0400

    add deletePoint method to the Model

--- src/model/points.js
+++ src/model/points.js
@@ -36,4 +36,18 @@ export default class Points {
     this._notify(updateType, update);
   }
 
+  deletePoint(updateType, update) {
+    const index = this._points.findIndex((point) => point.id === update.id);
+
+    if (index === -1) {
+      throw new Error(`Can't delete unexisting task`);
+    }
+
+    this._points = [
+      ...this._points.slice(0, index),
+      ...this._points.slice(index + 1)
+    ];
+
+    this._notify(updateType);
+  }
 }
