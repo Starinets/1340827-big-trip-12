5d36e0881d8882e3f2c43a0eb761d136963d80f5
Starinets <tsn.ipad@gmail.com>
2020-09-04 00:04:42 -0400

    add update method to th model

--- src/model/points.js
+++ src/model/points.js
@@ -10,4 +10,18 @@ export default class Points {
   getPoints() {
     return this._points;
   }
+
+  updatePoint(update) {
+    const index = this._points.findIndex((point) => point.id === update.id);
+
+    if (index === -1) {
+      throw new Error(`Can't update unexisting task`);
+    }
+
+    this._points = [
+      ...this._points.slice(0, index),
+      update,
+      ...this._points.slice(index + 1)
+    ];
+  }
 }
