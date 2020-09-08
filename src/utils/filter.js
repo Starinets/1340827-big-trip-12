70e042b88e935fda7e4632689fbd5903ebe31059
Starinets <tsn.ipad@gmail.com>
2020-09-08 10:08:49 -0400

    filters set naming

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -1,7 +1,7 @@
 import {FilterType} from './../constants';
 
-export const filter = {
-  [FilterType.EVERYTHING]: (points) => points,
+export const filterTypeToPoints = {
+  [FilterType.EVERYTHING]: (points) => points.slice(),
   [FilterType.FUTURE]: (points, currentDate) => points.filter((point) => point.startTime > currentDate),
   [FilterType.PAST]: (points, currentDate) => points.filter((point) => point.startTime <= currentDate)
 };
