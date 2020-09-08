abd543b77b62ebcc50fe1a875344f460f81d5151
Starinets <tsn.ipad@gmail.com>
2020-09-08 11:56:19 -0400

    fix Filter Presenter naming

--- src/presenter/filter.js
+++ src/presenter/filter.js
@@ -3,47 +3,47 @@ import {render, RenderPosition, replace, remove} from './../utils/dom';
 import {UpdateType} from './../constants';
 
 export default class Filter {
-  constructor(filterContainer, filterModel, pointsModel) {
-    this._filterContainer = filterContainer;
-    this._filterModel = filterModel;
+  constructor(container, model, pointsModel) {
+    this._container = container;
+    this._model = model;
     this._pointsModel = pointsModel;
-    this._currentFilter = null;
+    this._current = null;
 
-    this._filterComponent = null;
+    this._component = null;
 
     this._handleModelEvent = this._handleModelEvent.bind(this);
-    this._handleFilterTypeChange = this._handleFilterTypeChange.bind(this);
+    this._handleTypeChange = this._handleTypeChange.bind(this);
 
     this._pointsModel.addObserver(this._handleModelEvent);
-    this._filterModel.addObserver(this._handleModelEvent);
+    this._model.addObserver(this._handleModelEvent);
   }
 
   init() {
-    this._currentFilter = this._filterModel.getFilter();
+    this._current = this._model.getFilter();
 
-    const prevFilterComponent = this._filterComponent;
+    const prevComponent = this._component;
 
-    this._filterComponent = new FilterView(this._currentFilter);
-    this._filterComponent.setFilterTypeChangeHandler(this._handleFilterTypeChange);
+    this._component = new FilterView(this._current);
+    this._component.setFilterTypeChangeHandler(this._handleTypeChange);
 
-    if (prevFilterComponent === null) {
-      render(this._filterContainer, this._filterComponent, RenderPosition.BEFORE_END);
+    if (prevComponent === null) {
+      render(this._container, this._component, RenderPosition.BEFORE_END);
       return;
     }
 
-    replace(this._filterComponent, prevFilterComponent);
-    remove(prevFilterComponent);
+    replace(this._component, prevComponent);
+    remove(prevComponent);
   }
 
   _handleModelEvent() {
     this.init();
   }
 
-  _handleFilterTypeChange(filterType) {
-    if (this._currentFilter === filterType) {
+  _handleTypeChange(filterType) {
+    if (this._current === filterType) {
       return;
     }
 
-    this._filterModel.setFilter(UpdateType.MINOR, filterType);
+    this._model.setFilter(UpdateType.MINOR, filterType);
   }
 }
