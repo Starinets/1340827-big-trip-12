5b4a35a06ada90dc3fd110bfc47a64e76d1a266c
Starinets <tsn.ipad@gmail.com>
2020-09-09 18:49:53 -0400

    logic of Menu Component

--- src/view/menu.js
+++ src/view/menu.js
@@ -1,16 +1,44 @@
 import Abstract from './abstract';
 
+const ACTIVE_MENU_CLASS = `trip-tabs__btn--active`;
+
 const createMenuTemplate = () => {
   return (
     `<nav class="trip-controls__trip-tabs  trip-tabs">
-      <a class="trip-tabs__btn  trip-tabs__btn--active" href="#">Table</a>
+      <a class="trip-tabs__btn trip-tabs__btn--active" href="#">Table</a>
       <a class="trip-tabs__btn" href="#">Stats</a>
     </nav>`
   );
 };
 
 export default class Menu extends Abstract {
+  constructor() {
+    super();
+
+    this._tabsClickHandler = this._tabsClickHandler.bind(this);
+  }
+
   _getTemplate() {
     return createMenuTemplate();
   }
+
+  setClickHandler(callback) {
+    this._callback.itemClick = callback;
+    this.getElement().addEventListener(`click`, this._tabsClickHandler);
+  }
+
+  _tabsClickHandler(evt) {
+    evt.preventDefault();
+
+    const previousActive = this.getElement().querySelector(`.${ACTIVE_MENU_CLASS}`);
+    const currentActive = evt.target;
+
+    if (previousActive !== currentActive) {
+      previousActive.classList.remove(ACTIVE_MENU_CLASS);
+      currentActive.classList.add(ACTIVE_MENU_CLASS);
+
+      this._callback.itemClick();
+    }
+  }
+
 }
