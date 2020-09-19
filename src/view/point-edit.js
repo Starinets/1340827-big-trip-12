a276d449d190bf9fd0882982f990570800c06af2
Starinets <gobvoz@gmail.com>
2020-09-19 12:05:12 -0400

    added the ability to block the edit form

--- src/view/point-edit.js
+++ src/view/point-edit.js
@@ -83,7 +83,7 @@ const createOfferListTemplate = (pointData, offers) => {
   return (
     offers.map((offer, index) => {
       return (`<div class="event__offer-selector">
-        <input class="event__offer-checkbox  visually-hidden" id="event-offer-${index}-1" type="checkbox" data-title="${offer.title}" data-price="${offer.price}" name="event-offer-${index}" ${offer.checked ? `checked` : ``}>
+        <input class="event__offer-checkbox  visually-hidden" id="event-offer-${index}-1" type="checkbox" data-title="${offer.title}" data-price="${offer.price}" name="event-offer-${index}" ${offer.checked ? `checked` : ``} ${pointData.isDisabled ? `disabled` : ``}>
         <label class="event__offer-label" for="event-offer-${index}-1">
           <span class="event__offer-title">${offer.title}</span>
           +
@@ -97,7 +97,7 @@ const createOfferListTemplate = (pointData, offers) => {
 const createFavoriteButtonTemplate = (pointData, editablePoint) => {
   if (editablePoint === EditablePoint.OLD) {
     return (
-      `<input id="event-favorite-1" class="event__favorite-checkbox  visually-hidden" type="checkbox" name="event-favorite" ${pointData.isFavorite ? `checked` : ``}></input>
+      `<input id="event-favorite-1" class="event__favorite-checkbox  visually-hidden" type="checkbox" name="event-favorite" ${pointData.isFavorite ? `checked` : ``} ${pointData.isDisabled ? `disabled` : ``}></input>
       <label class="event__favorite-btn" for="event-favorite-1">
         <span class="visually-hidden">Add to favorite</span>
         <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
@@ -121,7 +121,7 @@ const createPointFormTemplate = (pointData, destinations, offers, editablePoint)
             <span class="visually-hidden">Choose event type</span>
             <img class="event__type-icon" width="17" height="17" src="img/icons/${pointData.type}.png" alt="Event type icon">
           </label>
-          <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox">
+          <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox" ${pointData.isDisabled ? `disabled` : ``}>
 
           <div class="event__type-list">
             <fieldset class="event__type-group">
@@ -144,7 +144,7 @@ const createPointFormTemplate = (pointData, destinations, offers, editablePoint)
           <label class="event__label  event__type-output" for="event-destination-1">
             ${pointTypeToPretext[pointData.type]}
           </label>
-          <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${pointData.destination.name}" list="destination-list-1">
+          <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${pointData.destination.name}" list="destination-list-1" ${pointData.isDisabled ? `disabled` : ``}>
           <datalist id="destination-list-1">
             ${optionsListTemplate}
           </datalist>
@@ -154,12 +154,12 @@ const createPointFormTemplate = (pointData, destinations, offers, editablePoint)
           <label class="visually-hidden" for="event-start-time-1">
             From
           </label>
-          <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="${dateToString(pointData.startTime)}">
+          <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="${dateToString(pointData.startTime)}" ${pointData.isDisabled ? `disabled` : ``}>
           —
           <label class="visually-hidden" for="event-end-time-1">
             To
           </label>
-          <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="${dateToString(pointData.endTime)}">
+          <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="${dateToString(pointData.endTime)}" ${pointData.isDisabled ? `disabled` : ``}>
         </div>
 
         <div class="event__field-group  event__field-group--price">
@@ -167,15 +167,15 @@ const createPointFormTemplate = (pointData, destinations, offers, editablePoint)
             <span class="visually-hidden">Price</span>
             €
           </label>
-          <input class="event__input  event__input--price" id="event-price-1" type="number" name="event-price" value="${pointData.price}">
+          <input class="event__input  event__input--price" id="event-price-1" type="number" name="event-price" value="${pointData.price}" ${pointData.isDisabled ? `disabled` : ``}>
         </div>
 
-        <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
-        <button class="event__reset-btn" type="reset">${editablePoint === EditablePoint.NEW ? `Cancel` : `Delete`}</button>
+        <button class="event__save-btn  btn  btn--blue" type="submit" ${pointData.isDisabled ? `disabled` : ``}>Save</button>
+        <button class="event__reset-btn" type="reset" ${pointData.isDisabled ? `disabled` : ``}>${editablePoint === EditablePoint.NEW ? `Cancel` : `Delete`}</button>
 
         ${createFavoriteButtonTemplate(pointData, editablePoint)}
 
-        <button class="event__rollup-btn" type="button">
+        <button class="event__rollup-btn" type="button" ${pointData.isDisabled ? `disabled` : ``}>
           <span class="visually-hidden">Open event</span>
         </button>
       </header>
@@ -454,11 +454,16 @@ export default class PointEdit extends SmartView {
   static parsePointToData(point) {
     return Object.assign(
         {},
-        point
+        point,
+        {
+          isDisabled: false,
+        }
     );
   }
 
   static parseDataToPoint(pointData) {
+    delete pointData.isDisabled;
+
     return Object.assign(
         {},
         pointData
