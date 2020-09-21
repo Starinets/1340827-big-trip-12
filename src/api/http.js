a64fb5cb2d9fd3c053470db8c29bd3f103048460
Starinets <gobvoz@gmail.com>
2020-09-21 14:30:14 -0400

    implemented sync method of http api

--- src/api/http.js
+++ src/api/http.js
@@ -72,6 +72,16 @@ export default class Http {
     });
   }
 
+  sync(data) {
+    return this._load({
+      url: `points/sync`,
+      method: Method.POST,
+      body: JSON.stringify(data),
+      headers: new Headers({"Content-Type": `application/json`})
+    })
+      .then(Http.toJSON);
+  }
+
   _load({
     url,
     method = Method.GET,
